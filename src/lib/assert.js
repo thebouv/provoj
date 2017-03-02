3a97daf46394d0b83db47701d8e1d7c2b974b46e
Dinarte Jesus <jdinartejesus@gmail.com>
2017-03-02 21:30:29 +0100

    Updated asserting file

--- src/lib/assert.js
+++ src/lib/assert.js
@@ -1,11 +1,12 @@
-module.exports = {
-  _assert (value, msg) {
-    if (!value) {
-      throw new Error(msg || `AssertionError: when evaluation ${value}`)
-    }
-  },
-  equal (value1, value2, msg) {
-    msg = msg || `AssertionError: expected ${value1} to equal ${value2}`
-    this._assert(value1 === value2, msg)
+function assert (value, msg) {
+  if (!value) {
+    throw new Error(msg || `AssertionError: when evaluation ${value}`)
   }
 }
+
+assert.equal = function (value1, value2, msg) {
+  msg = msg || `AssertionError: expected ${value1} to equal ${value2}`
+  assert(value1 === value2, msg)
+}
+
+module.exports = assert
